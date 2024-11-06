9cf88a557cf1b9bf172e9cae0d5bf6d07c22d3c1
aj <balajivijay679@gmail.com>
2024-11-06 21:51:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -15,39 +15,39 @@ const CATEGORIES = [
   // { name: "others", color: "black" },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
+// const initialFacts = [
+//   {
+//     id: 1,
+//     text: "React is being developed by Meta (formerly facebook)",
+//     source: "https://opensource.fb.com/",
+//     category: "technology",
+//     votesInteresting: 24,
+//     votesMindblowing: 9,
+//     votesFalse: 4,
+//     createdIn: 2021,
+//   },
+//   {
+//     id: 2,
+//     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+//     source:
+//       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+//     category: "society",
+//     votesInteresting: 11,
+//     votesMindblowing: 2,
+//     votesFalse: 0,
+//     createdIn: 2019,
+//   },
+//   {
+//     id: 3,
+//     text: "Lisbon is the capital of Portugal",
+//     source: "https://en.wikipedia.org/wiki/Lisbon",
+//     category: "society",
+//     votesInteresting: 8,
+//     votesMindblowing: 3,
+//     votesFalse: 1,
+//     createdIn: 2015,
+//   },
+// ];
 /********************************heder***************************/
 
 
@@ -64,6 +64,7 @@ function App() {
         .select("*")
         // .eq("category", cate);
       console.log(community);
+      console.log(error);
       setallfacts(community);
       // console.log(error);
     }
@@ -131,6 +132,7 @@ function Factform({ setallfacts, setShowForm }) {
         .from("info")
         .insert([{text,source, category:cate }])
         .select();
+        console.log(error);
       if (newFact && newFact.length > 0) {
         setallfacts((facts) => [{ id: newFact[0].id, ...newFact[0] },...facts]);
       }
@@ -198,7 +200,7 @@ function Factlist({ allfact }) {
       <li key={allfact.id} className="fact">
         <p>
           {allfact.text}
-          <a className="link" href={allfact.source} target="_blank">
+          <a className="link" href={allfact.source} rel="noreferrer" target="_blank">
             (source)
           </a>
         </p>
